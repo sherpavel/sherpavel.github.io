224dd0d5cfca3d9b44c57a338210f070b34ce4c8
Pavel Sher <sher00pave@gmail.com>
2021-09-12 00:36:00 -0400

    Mobile overhaul
    Added:
    * Arrow wave animation
    
    Changed:
    * Mobile CSS

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,20 +1,20 @@
 const projects = [
     {
         name: "Gradient",
-        img: "valley_dark.png",
-        text: "Gradient tool is a simple and intuitive preview for a multi-color gradient. The website is mobile-friendly and has a multitude of sharing options that are useful for both web and application developers.",
+        img: "valley_new.png",
+        text: "Gradient is a simple and intuitive preview tool for multi-color gradients. The website is mobile-friendly and has a multitude of sharing options that are useful for web and software developers.",
         link: "https://sherpavel.github.io/Gradient/"
     },
     {
-        name: "Portfolio",
+        name: "This page",
         img: "logo.png",
         text: "This website is my portfolio and a project on it’s own. If you are interested in how it works, click to visit the GitHub page.",
         link: "https://github.com/sherpavel/sherpavel.github.io"
     },
     {
         name: "Argon",
-        img: "valley_new.png",
-        text: "Argon is a powerful Java framework with UI for code visualization. It provides all the necessary functionality for displaying and interactivity<br>The background and logos for this website was made with Argon. To learn more, visit the GitHub page.<br>(To be released)",
+        img: "argon_icon.png",
+        text: "Argon is a powerful Java framework with UI for code visualization. It provides the functionality for drawing, interactivity and recording.<br>The background and logos for this website were made with Argon. To try yourself and learn more, visit the GitHub page.<br>(To be released)",
         link: null
     },
 ];
